21c6955c7ccff7a8ac7e84b7ead5956bc0580bbb
karti <kartikkumarji24@gmail.com>
2024-10-12 01:27:56 +0530

    lock

--- src/BankAcount.java
+++ src/BankAcount.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAcount {
+    private int balance = 100;
+    private Lock lock = new ReentrantLock();
+
+    public void withdraw(int amount) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + "Attempting to withdraw" + amount);
+        try {
+            if (lock.tryLock(3000, TimeUnit.MILLISECONDS)) {
+                if (amount <= balance) {
+                    try {
+                        System.out.println(Thread.currentThread().getName() + "proceeding to withdraw" + amount);
+                        Thread.sleep(5000);
+                        balance -= amount;
+                        System.out.println(Thread.currentThread().getName() + "completed the withdraw. Remaining amount" + balance);
+                    } catch (Exception e) {
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    System.out.println(Thread.currentThread().getName() + "Insufficient");
+                }
+
+            } else {
+                System.out.println(Thread.currentThread().getName() + " could not acquire");
+            }
+        }catch (Exception e){
+
+        }
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        BankAcount SBI = new BankAcount();
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    SBI.withdraw(50);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
 
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Thread t1 = new Thread(task, "Thread 1");
+        Thread t2 = new Thread(task, "Thread 2");
+        t1.start();
+        t2.start();
     }
 }
